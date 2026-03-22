4d7d29eb7c414633af66095b48baadc305ef7d2f
marlene-dev <kamdemraissa9@gmail.com>
2026-03-22 09:51:18 +0100

    création manuelle bucket dans aws

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-marlene-12345"
+    bucket       = "terraform-state-bucket-project01"
     key          = "project01/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
-    use_lockfile = true
+    dynamodb_table = "terraform-locks"
   }
 }
