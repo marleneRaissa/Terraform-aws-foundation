9370fa3b1d2de7357127dfe5f9ab295a6cb73d7e
marlene-dev <kamdemraissa9@gmail.com>
2026-03-22 17:26:58 +0100

    move file in lab directory

--- locals.tf
+++
@@ -1,4 +0,0 @@
-# Liste de tous les IDs de subnets Prend le premier élément de la liste, Stocke cet ID dans une variable locale nommée first_subnet_id
-locals {
-  first_subnet_id = data.aws_subnets.list_subnet.ids[0]
-}
