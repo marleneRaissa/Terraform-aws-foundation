e43625df48209ad19d92f47c80e62a59c7c1a3c9
marlene-dev <kamdemraissa9@gmail.com>
2026-03-23 02:26:21 +0100

    lab02/configuration des ressources

---
+++ vpc-lab02/main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source             = "./modules/vpc"
+  vpc_cidr           = "10.0.0.0/16"
+  environment        = "dev"
+  project_name       = "lab02-full-vpc"
+  availability_zones = ["us-east-1a", "us-east-1b"] // do not mix Regions (us-east-1), and AZ (us-east-1a)
+}
