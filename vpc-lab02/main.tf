a8fd2d4fb5711e82a63d2ca7551dfe8ac3d6744f
marlene-dev <kamdemraissa9@gmail.com>
2026-03-23 12:38:43 +0100

    lab02/maj valeur  availability_zone

--- vpc-lab02/main.tf
+++ vpc-lab02/main.tf
@@ -3,5 +3,5 @@ module "vpc" {
   vpc_cidr           = "10.0.0.0/16"
   environment        = "dev"
   project_name       = "lab02-full-vpc"
-  availability_zone = ["us-east-1a", "us-east-1b"] // do not mix Regions (us-east-1), and AZ (us-east-1a)
+  availability_zone = ["eu-north-1a", "eu-north-1b"] // do not mix Regions (us-east-1), and AZ (eu-north-1a)
 }
