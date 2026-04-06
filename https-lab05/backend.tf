851071c6609bd9dc6a16937e26e7873fc330e6f6
marlene-dev <kamdemraissa9@gmail.com>
2026-04-07 01:10:29 +0200

    lab05/correct bucket path name

--- https-lab05/backend.tf
+++ https-lab05/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-state-bucket-s3-project01"
-    key          = "lab04/alb/terraform.tfstate"
+    key          = "lab05/alb/terraform.tfstate"
     region       = "eu-north-1"
     encrypt      = true
     use_lockfile = true
