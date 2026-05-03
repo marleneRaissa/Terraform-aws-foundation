99ee1f1c749b64ef909da747ec49c35b8e4ef64b
marlene-dev <kamdemraissa9@gmail.com>
2026-05-03 22:15:59 +0200

    lab06/ajout commentaire

--- autoscaling-lab06/user_data.sh
+++ autoscaling-lab06/user_data.sh
@@ -1,3 +1,5 @@
+# ce script permet de vérifier que : ALB fonctionne, Target Group fonctionne, ASG crée bien les instances, Apache répond bien, le trafic arrive bien sur les instances
+
 #!/bin/bash
 set -eux
 
@@ -11,7 +13,22 @@ yum install -y httpd
 systemctl enable httpd
 systemctl start httpd
 
+# Get EC2 metadata token for IMDSv2
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+# Get instance ID using IMDSv2 token
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/instance-id)
+
+
+# Pourquoi afficher l’Instance ID ? C’est très utile avec un Auto Scaling Group + ALB.
+# Si ton ALB envoie le trafic vers plusieurs instances, tu peux rafraîchir la page 
+# plusieurs fois et voir si tu tombes sur différentes instances : 
+# Refresh 1 → Instance ID: i-aaa111, Refresh 2 → Instance ID: i-bbb222, Refresh 3 → Instance ID: i-ccc333
+
 # Create demo index page (useful for ASG + ALB validation)
+# Demande au service de métadonnées AWS l’ID de cette instance EC2
 cat <<EOF > /var/www/html/index.html
 <!DOCTYPE html>
 <html>
@@ -20,7 +37,7 @@ cat <<EOF > /var/www/html/index.html
   </head>
   <body>
     <h1>Hello from Terraform Day 12 ASG</h1>
-    <p><strong>Instance ID:</strong> $(curl -s http://169.254.169.254/latest/meta-data/instance-id)</p>
+    <p><strong>Instance ID:</strong> ${INSTANCE_ID}</p>
   </body>
 </html>
 EOF
