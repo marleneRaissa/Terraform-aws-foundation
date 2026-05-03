8eb76848a8c7d8172f3247487720a336d7a799e4
marlene-dev <kamdemraissa9@gmail.com>
2026-05-03 23:19:25 +0200

    lab06/maj du nom du backend

--- autoscaling-lab06/backend.tf
+++ autoscaling-lab06/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-state-bucket-s3-project01"
-    key          = "lab06/alb/terraform.tfstate"
+    key          = "lab06/autoscaling/terraform.tfstate"
     region       = "eu-north-1"
     encrypt      = true
     use_lockfile = true
