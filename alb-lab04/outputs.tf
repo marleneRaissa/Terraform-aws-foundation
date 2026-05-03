3e9600064b58a4871d778542b02506d361800788
marlene-dev <kamdemraissa9@gmail.com>
2026-05-03 22:14:58 +0200

    lab04/ajout commentaire

--- alb-lab04/outputs.tf
+++ alb-lab04/outputs.tf
@@ -6,6 +6,8 @@ output "alb_arn" {
   value = aws_lb.alb.arn
 }
 
+# il permettra ensuite à d’autres ressources dans le lab06 , comme un Auto Scaling Group, de dire :
+# “Mes instances doivent être ajoutées dans ce Target Group de l’ALB.
 output "app_tg_arn" {
   value = aws_lb_target_group.app_tg.arn
 }
